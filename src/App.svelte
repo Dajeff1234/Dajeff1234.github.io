7c876837e84c61eb52af4290a3fe468674fad78b
Dajeff1234 <113061490+Dajeff1234@users.noreply.github.com>
2022-11-14 11:32:08 -0800

    ASDF

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,14 @@
 <script>
   // @ts-ignore
   import no from "./lib/Navbar.svelte"
+
+  export let thing = "#313131";
 </script>
+<style>
+  .body{
+    background-color: {thing };
+  }
+</style>
 <main>
   hello
 </main>
