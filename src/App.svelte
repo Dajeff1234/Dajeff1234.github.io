408e237e34a341389914c6464ab4aaae78b67b08
Dajeff1234 <113061490+Dajeff1234@users.noreply.github.com>
2022-11-21 11:31:28 -0800

    awefad

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,32 @@
 <script>
+  import { fly } from './transitions';
   import Navbar from "./lib/Navbar.svelte";
+  import { onMount } from 'svelte';
+  let ready = false;
+  onMount(() => ready = true);
 </script>
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+</svelte:head>
 <style>
   
   :global(body) {
     margin: 0;
   }
+  :root{
+    font-family: 'Montserrat', sans-serif !important;
+  }
+  .welcom{
+    text-align: center;``
+    font-size: 50px;
+  }
 </style>
 
 <main>
   <Navbar/>
-  no your mom
-  
+  {#if ready}
+  <div class = "welcom" transition:fly={{ x: '1500%' }}>welcom</div>
+  {/if}
 </main>
