ca623e2e5077deb91ce620f53138330822965195
Dajeff1234 <113061490+Dajeff1234@users.noreply.github.com>
2022-12-01 17:54:04 -0800

    FAVICON GO BRRRR

--- src/App.svelte
+++ src/App.svelte
@@ -7,10 +7,12 @@
   let ready2 = false;
 </script>
 <svelte:head>
+  <link rel="icon" type="image/x-icon" href="src/lib/icons/f2.ico">
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
 </svelte:head>
+<link rel="icon" type="image/x-icon" href="src/lib/icons/f2.ico">
 <style>
   
   :global(body) {
