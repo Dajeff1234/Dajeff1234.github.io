eaccedeac3cebee65a88d99473326687e68b8843
Dajeff1234 <113061490+Dajeff1234@users.noreply.github.com>
2022-12-05 14:27:16 -0800

    PIDUjkhaljk

--- src/lib/themeSwitch.svelte
+++ src/lib/themeSwitch.svelte
@@ -1,63 +1,77 @@
 
 <script>
+    import { transition_in } from "svelte/internal";
+
+
 
     export let theme = true
     
-    $: if (typeof document == 'object') {
+    /*$: if (typeof document == 'object') {
         document.documentElement.classList.toggle('dark', theme)
-    }
+    }*/
 </script>
-<!-- <svelte:head>
+<svelte:head>
     {#if theme}
   <style>
-        :root {
-            background: rgb(36, 36, 36);
-            color: rgb(212, 212, 212);
-        } 
-        .Button{
-            color: rgb(212, 212, 212);
-        }
-    </style>
-  {:else }
-    <style>
-    :root {
-        background: rgb(235, 235, 235); 
-        color: black;
-        } 
-        .nav-cont{
-            background-color: rgb(209, 209, 209) !important;
-        }
-    </style>
-  {/if}
-
-</svelte:head> -->
-
-<li><button  class="Button" on:click="{() => theme = !theme}">theme</button></li>
-<style>
-
-    :root {
-        font-family: 'Montserrat', sans-serif !important;
+        :root{
         transition: 1s;
-        background: rgb(36, 36, 36);
-        color: rgb(212, 212, 212);
-    }
-
-    :root.dark {
         background: rgb(235, 235, 235); 
         color: black;
     }
-
-    .dark .nav-cont {
-        transition: 1s;
-        background-color: rgb(209, 209, 209) !important;
-    }
-
-
+        
     .Button{
+        transition: 1s;
         font-family: 'Montserrat', sans-serif;
         background:none;
         border:none;
         color: rgb(212, 212, 212);
         
     }
+    .newSection{
+    transition: 1s;
+    background-color: rgb(203, 203, 203);
+    text-align: center;
+    padding-bottom:55%;
+  }
+  .stuff{
+    transition:1s !important;
+  }
+    </style>
+  {:else }
+    <style>
+    :root {
+        font-family: 'Montserrat', sans-serif !important;
+        transition: 1s;
+        background: rgb(36, 36, 36);
+        color: rgb(212, 212, 212);
+    } 
+    .Button{
+        transition: 1s;
+        font-family: 'Montserrat', sans-serif;
+        background:none;
+        border:none;
+        color: rgb(212, 212, 212);
+        
+    }
+    
+  .newSection{
+    transition:1s;
+    background-color: rgb(65, 65, 65) !important;
+    text-align: center;
+    padding-bottom:55%;
+    }
+    </style>
+  {/if}
+
+</svelte:head>
+
+<li><button  class="Button" on:click="{() => theme = !theme}">theme</button></li>
+<style>
+
+    
+
+
+
+
+
 </style>
